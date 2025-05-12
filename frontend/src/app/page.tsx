2ea3000a3d9626418f867c74476e53bf0708113d
Keylab <keylabmichelle@gmail.com>
2025-05-11 20:14:23 -0600

    fix: cleaned up vite tailwind

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className = "min-h-screen text-white flex items-center justify-center">
+    <main className = "min-h-screen text-white flex items-center justify-center bg-background">
 
       <h1 className = "text-4xl font-bold"> Web app </h1>
 
