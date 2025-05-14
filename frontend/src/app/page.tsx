10b3d8648086ae95a4b1b94869982f0c20394985
Keylab <keylabmichelle@gmail.com>
2025-05-14 14:09:12 -0600

    Fix: Fixed grid filling

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,23 +4,23 @@ export default function Home() {
   return (
 
 
-    <main className="min-h-screen pb-16 bg-[url('/green-robot.png')] bg-no-repeat bg-center bg-contain text-white p-10  z-10 grid grid-flow-col grid-cols-4  gap-2">
+    <main className="min-h-screen pb-16 bg-[url('/green-robot.png')] bg-no-repeat bg-center bg-contain text-white p-10 relative">
 
-      <div className='hover:bg-tertiary rounded-md h-20 w-20 justify-center flex flex-col items-center cursor-pointer'>
-        <Image src="/icons/folder.png" alt="Folder" width={50} height={50} />
-        <h3>Games</h3>
+      <div className=" grid grid-flow-col auto-rows-min grid-rows-4 sm:grid-rows-6 md:grid-rows-8 gap-3  max-w-[30vh] ">
 
+        <div className='hover:bg-tertiary rounded-md h-20 w-20 justify-center flex flex-col items-center cursor-pointer'>
+          <Image src="/icons/folder.png" alt="Folder" width={50} height={50} />
+          <h3>Games</h3>
 
+        </div>
+
+        <div className='hover:bg-tertiary rounded-md h-20 w-20 justify-center flex flex-col items-center cursor-pointer'>
+          <Image src="/icons/text_file.png" alt="Text file" width={50} height={50} />
+          <h3 className='text-sm'>README.txt</h3>
+        </div>
+
+        
       </div>
-
-      <div className='hover:bg-tertiary rounded-md h-20 w-20 justify-center flex flex-col items-center cursor-pointer'>
-        <Image src="/icons/text_file.png" alt="Text file" width={50} height={50} />
-        <h3 className='text-sm'>README.txt</h3>
-
-
-      </div>
-
-
 
     </main>
 
